736c3c644d25537df07392f6abe95aed39d01578
Mark Thompson <mst101@users.noreply.github.com>
2025-03-26 12:05:13 +0000

    Update deps

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,10 +1,10 @@
 import type { DefineComponent } from 'vue';
-import { createSSRApp, h } from 'vue';
-import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
+import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from 'ziggy-js';
+import { createSSRApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,7 +16,7 @@ createServer(page =>
         resolve: name =>
             resolvePageComponent(
                 `./Pages/${name}.vue`,
-                import.meta.glob < DefineComponent > ('./Pages/**/*.vue'),
+                import.meta.glob <DefineComponent> ('./Pages/**/*.vue'),
             ),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
